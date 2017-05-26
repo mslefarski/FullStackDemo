4c0fbca1014c798b2f081a459d6c6ac13220fef8
Mikolaj Slefarski <mikolaj.slefarski@gmail.com>
2017-05-26 15:40:05 +0200

    com1

---
+++ src/main/java/FullStackDemoStarter.java
@@ -0,0 +1,42 @@
+package main.java;
+import java.io.Reader;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import main.java.model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.*;
+
+/**
+ * Created by m.slefarski on 2017-05-26.
+ */
+public class FullStackDemoStarter {
+
+
+    public static void main(String args[]) throws Exception {
+        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        SqlSession session = sqlSessionFactory.openSession();
+
+        //data
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println("Current time: "+sdf.format(timestamp));
+
+
+        //Create user
+        User user = new User("user_login3", "user_email@gmail.com", "lol");
+
+        //Insert data
+        session.insert("User.insert", user);
+        System.out.println("User inserted successfully.");
+
+
+        //commit, close
+        session.commit();
+        session.close();
+
+    }
+
+
+}
